d5769a90d2a9a4e2faeaa273e2cddaa611ee10ef
Kor de Jong <k.dejong1@uu.nl>
2024-10-01 08:32:41 +0200

    WIP

--- source/framework/api/c/include/lue/framework/api/c.h
+++ source/framework/api/c/include/lue/framework/api/c.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "lue/framework/api/c/array.h"
 #include "lue/framework/api/c/field.h"
 #include "lue/framework/api/c/io.h"
 #include "lue/framework/api/c/local_operation.h"
+#include "lue/framework/api/c/runtime.h"

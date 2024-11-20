2657d43ad222786bd474619dde7ceea0880f7d94
Kor de Jong <k.dejong1@uu.nl>
2024-11-20 17:55:26 +0100

    Initial version, stubs

---
+++ source/data_model/netcdf/test/variable_test.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE lue netcdf variable
+#include "lue/netcdf/variable.hpp"
+#include <boost/test/included/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE(define)
+{
+    BOOST_CHECK(true);
+}

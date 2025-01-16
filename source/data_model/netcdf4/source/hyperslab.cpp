567d5b6c923a9d446fb915c383a7e055d1e3e479
Kor de Jong <k.dejong1@uu.nl>
2025-01-16 08:24:17 +0100

    WIP

---
+++ source/data_model/netcdf4/source/hyperslab.cpp
@@ -0,0 +1,84 @@
+#include "lue/netcdf4/hyperslab.hpp"
+#include <algorithm>
+#include <cassert>
+
+
+namespace lue::netcdf {
+
+    /*!
+        @brief      Construct an instance given @a starts, @a counts, and @a strides
+
+        Strides must be larger than zero. If the strides for all dimensions are one then the hyperslab is
+        non-strided.
+    */
+    Hyperslab::Hyperslab(Starts&& starts, Counts&& counts, Strides&& strides):
+
+        _starts{std::move(starts)},
+        _counts{std::move(counts)},
+        _strides{std::move(strides)}
+
+    {
+        assert(_counts.size() == _starts.size());
+        assert(_strides.size() == _starts.size());
+
+        // A stride of zero makes no sense
+        assert(std::all_of(_strides.begin(), _strides.end(), [](auto const stride) { return stride > 0; }));
+    }
+
+
+    /*!
+        @brief      Construct a non-strided instance
+    */
+    Hyperslab::Hyperslab(Starts&& starts, Counts&& counts):
+
+        Hyperslab{std::move(starts), std::move(counts), Strides(starts.size(), 1)}
+
+    {
+    }
+
+
+    /*!
+        @brief      Return the starts
+    */
+    auto Hyperslab::starts() const -> Starts
+    {
+        return _starts;
+    }
+
+
+    /*!
+        @brief      Return the counts
+    */
+    auto Hyperslab::counts() const -> Counts
+    {
+        return _counts;
+    }
+
+
+    /*!
+        @brief      Return the strides
+    */
+    auto Hyperslab::strides() const -> Strides
+    {
+        return _strides;
+    }
+
+
+    /*!
+        @brief      Return the number of dimensions
+    */
+    auto Hyperslab::nr_dimensions() const -> int
+    {
+        return static_cast<int>(_starts.size());
+    }
+
+
+    /*!
+        @brief      Return whether the hyperslab defines a strided subset, where elements are skipped
+    */
+    auto Hyperslab::is_strided() const -> bool
+    {
+        return std::all_of(_strides.begin(), _strides.end(), [](auto const lhs) { return lhs == 1; });
+    }
+
+}  // namespace lue::netcdf
--- source/data_model/netcdf4/test/attribute_test.cpp
+++ source/data_model/netcdf4/test/attribute_test.cpp
@@ -1,9 +1,85 @@
 #define BOOST_TEST_MODULE lue netcdf4 attribute
 #include "lue/netcdf4/attribute.hpp"
+#include "lue/netcdf4/dataset.hpp"
 #include <boost/test/included/unit_test.hpp>
 
 
-BOOST_AUTO_TEST_CASE(define)
+namespace {
+
+    template<lue::Arithmetic T>
+    void test_array(
+        lue::netcdf::Group& group, std::string const& name, std::size_t const nr_values, T const* values)
+    {
+        BOOST_REQUIRE(!group.has_attribute(name));
+
+        {
+            auto const attribute = group.add_attribute(name, nr_values, values);
+            BOOST_CHECK_EQUAL(attribute.type(), lue::netcdf::TypeTraits<T>::type_id);
+            BOOST_REQUIRE_EQUAL(attribute.length(), nr_values);
+            std::vector<T> values_read(nr_values);
+            attribute.values(values_read.data());
+            BOOST_CHECK_EQUAL_COLLECTIONS(values_read.begin(), values_read.end(), values, values + nr_values);
+        }
+
+        BOOST_REQUIRE(group.has_attribute(name));
+        auto const attribute = group.attribute(name);
+        BOOST_CHECK_EQUAL(attribute.type(), lue::netcdf::TypeTraits<T>::type_id);
+        BOOST_REQUIRE_EQUAL(attribute.length(), nr_values);
+        std::vector<T> values_read(nr_values);
+        attribute.values(values_read.data());
+        BOOST_CHECK_EQUAL_COLLECTIONS(values_read.begin(), values_read.end(), values, values + nr_values);
+    }
+
+
+    template<lue::Arithmetic T>
+    void test_scalar(lue::netcdf::Group& group, std::string const& name, T const value)
+    {
+        test_array(group, name, 1, &value);
+    }
+
+
+    template<lue::Arithmetic T>
+    void test_array(
+        lue::netcdf::Group& group, std::string const& name, std::size_t const nr_values, T const value)
+    {
+        std::vector<T> const values(nr_values, value);
+        test_array(group, name, nr_values, values.data());
+    }
+
+}  // Anonymous namespace
+
+
+BOOST_AUTO_TEST_CASE(scalar)
 {
-    BOOST_CHECK(true);
+    std::string const dataset_name = "attribute_scalar.nc";
+    auto dataset = lue::netcdf::Dataset::create(dataset_name, NC_CLOBBER);
+
+    test_scalar(dataset, "attribute_int8", std::int8_t{-8});
+    test_scalar(dataset, "attribute_uint8", std::uint8_t{8});
+    test_scalar(dataset, "attribute_int32", std::int32_t{-32});
+    test_scalar(dataset, "attribute_uint32", std::uint32_t{32});
+    test_scalar(dataset, "attribute_int64", std::int64_t{-64});
+    test_scalar(dataset, "attribute_uint64", std::uint64_t{64});
+    test_scalar(dataset, "attribute_float", float{5.5});
+    test_scalar(dataset, "attribute_double", double{6.6});
+
+    // TODO string
+}
+
+
+BOOST_AUTO_TEST_CASE(array)
+{
+    std::string const dataset_name = "attribute_array.nc";
+    auto dataset = lue::netcdf::Dataset::create(dataset_name, NC_CLOBBER);
+
+    test_array(dataset, "attribute_int8", 5, std::int8_t{-8});
+    test_array(dataset, "attribute_uint8", 5, std::uint8_t{8});
+    test_array(dataset, "attribute_int32", 5, std::int32_t{-32});
+    test_array(dataset, "attribute_uint32", 5, std::uint32_t{32});
+    test_array(dataset, "attribute_int64", 5, std::int64_t{-64});
+    test_array(dataset, "attribute_uint64", 5, std::uint64_t{64});
+    test_array(dataset, "attribute_float", 5, float{5.5});
+    test_array(dataset, "attribute_double", 5, double{6.6});
+
+    // TODO string
 }

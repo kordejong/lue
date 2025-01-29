208c582d1f0476343e720b8d5c44bdb1101a6841
Kor de Jong <k.dejong1@uu.nl>
2025-01-29 09:02:04 +0100

    WIP CF data model

---
+++ source/data_model/cf/include/lue/cf/netcdf/dimension.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "lue/netcdf4/dimension.hpp"
+
+
+namespace lue::cf::netcdf {
+
+    class Dimension: public lue::netcdf::Dimension
+    {
+
+        public:
+
+        private:
+    };
+
+}  // namespace lue::cf::netcdf

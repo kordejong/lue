237255c75c6927ab954389279ea078fad5d3e64e
Kor de Jong <k.dejong1@uu.nl>
2025-04-04 16:15:43 +0200

    WIP CF data model

---
+++ source/data_model/cf/include/lue/cf/netcdf/auxiliary_coordinate_variable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "lue/cf/netcdf/generic_coordinate_variable.hpp"
+
+
+namespace lue::cf::netcdf {
+
+    class AuxiliaryCoordinateVariable: public GenericCoordinateVariable
+    {
+
+        public:
+
+        private:
+    };
+
+}  // namespace lue::cf::netcdf

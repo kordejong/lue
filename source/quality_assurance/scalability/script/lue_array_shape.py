6f1e9fd247d34f4b60627f26535549a0501917ec
Kor de Jong <k.dejong1@uu.nl>
2025-07-15 13:08:49 +0200

    Fix shebang: python -> python3

--- source/quality_assurance/scalability/script/lue_array_shape.py
+++ source/quality_assurance/scalability/script/lue_array_shape.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Wrapper for the array_shape command-line utility
 """
--- source/quality_assurance/scalability/script/lue_scalability.py
+++ source/quality_assurance/scalability/script/lue_scalability.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Wrapper for the scalability command-line utility
 """

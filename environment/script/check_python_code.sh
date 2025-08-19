0c093310c960e0d3c6bd141a0d8599e6815d886a
Kor de Jong <k.dejong1@uu.nl>
2025-08-19 10:15:29 +0200

    Support commandline arguments

--- environment/script/check_python_code.sh
+++ environment/script/check_python_code.sh
@@ -1,22 +1,37 @@
 set -euo pipefail
 
-build_type=Debug
-build_type=Release
-lue_build_prefix="$OBJECTS/$build_type/lue"
-lue_python_prefix="$(echo "$lue_build_prefix"/lib*/python3.*)"
-
-export PYTHONPATH="$lue_python_prefix:${PYTHONPATH:-}"
-
-lue_ad_hoc_prefix="$lue_python_prefix/lue/command"
-
-echo "ruff..."
-ruff check "$lue_ad_hoc_prefix"
-echo "pyright..."
-pyright --project "$LUE" "$lue_ad_hoc_prefix"
-echo "mypy..."
-mypy --strict "$lue_ad_hoc_prefix"
-
-# Figure out whether it is better to use typings/lue/data_model/__init__.pyi
+# TODO: We dump .pyi files in the subpackage directories. Figure out whether it is better to use
+#   typings/lue/<subpackage>/__init__.pyi
 # - https://peps.python.org/pep-0484/#stub-files
 # - https://microsoft.github.io/pyright/#/type-stubs
 # - https://github.com/microsoft/pyright/issues/10089
+
+function parse_command_line() {
+    usage="$(basename $0) build_prefix"
+
+    if [[ $# != 1 ]]; then
+        echo $usage
+        exit 1
+    fi
+
+    lue_build_prefix=$1
+}
+
+function check_python_code() {
+    lue_python_prefix="$(echo "$lue_build_prefix"/lib*/python3.*)"
+
+    export PYTHONPATH="$lue_python_prefix:${PYTHONPATH:-}"
+
+    # TODO: Add directories, test all installed Python code
+    lue_ad_hoc_prefix="$lue_python_prefix/lue/command"
+
+    echo "ruff..."
+    ruff check "$lue_ad_hoc_prefix"
+    echo "pyright..."
+    pyright --project "$LUE" "$lue_ad_hoc_prefix"
+    echo "mypy..."
+    mypy --strict "$lue_ad_hoc_prefix"
+}
+
+parse_command_line $@
+check_python_code

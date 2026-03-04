f658e3ace70b07e6980cb53f629f6ab082d90a30
Kor de Jong <k.dejong1@uu.nl>
2026-03-04 13:58:18 +0100

    WIP add support for example kernels, pass-through value scale

--- source/document/python/package/document/figure/__init__.py
+++ source/document/python/package/document/figure/__init__.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from .map import create_map
 
 
-def plot(dataset_path: Path, figure_path: Path) -> None:
+def plot(dataset_path: Path, figure_path: Path, formats: list[str]) -> None:
 
     if dataset_path.suffix in [".tif"]:
-        create_map(dataset_path, figure_path)
+        create_map(dataset_path, figure_path, formats)
     else:
         raise ValueError(f"Don't know how to plot {dataset_path} yet")

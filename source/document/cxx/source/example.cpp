af03028702179d5a552d259ba6ac0afdfddbe7be
Kor de Jong <k.dejong1@uu.nl>
2026-03-05 17:09:05 +0100

    Refactor

--- source/document/cxx/source/example.cpp
+++ source/document/cxx/source/example.cpp
@@ -6,7 +6,22 @@ namespace lue::document {
 
     Example::Example(cxxopts::Options&& options, int argc, char const* const* argv):
 
-        utility::Application{std::move(options), argc, argv}
+        utility::Application{
+            [](cxxopts::Options&& options) -> cxxopts::Options
+            {
+                // Stuff common to all example's options
+                options.custom_help("");
+                options.add_options()
+                    // clang-format off
+                    ("h,help", "Show usage")
+                    ("v,version", "Show version")
+                    // clang-format on
+                    ;
+
+                return std::move(options);
+            }(std::move(options)),
+            argc,
+            argv}
 
     {
         lue::gdal::register_gdal_drivers();

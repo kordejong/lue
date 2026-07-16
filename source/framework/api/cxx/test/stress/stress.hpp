f3d956f7883423230f48472325c7ad8375064e91
Kor de Jong <k.dejong1@uu.nl>
2026-07-16 14:36:33 +0200

    Pass path as string to cxxopts::Options constructor

--- source/framework/api/cxx/test/stress/stress.hpp
+++ source/framework/api/cxx/test/stress/stress.hpp
@@ -49,7 +49,7 @@ namespace lue::stress {
                     {
                         cxxopts::Options options{
                             // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-                            std::filesystem::path{argv[0]}.filename(),
+                            std::filesystem::path{argv[0]}.filename().string(),
                             "Program to verify memory isn't increasing while iterating"};
 
                         options.custom_help("");
